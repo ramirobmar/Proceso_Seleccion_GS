e5ed2f1fb5f50079c34404d991884ac1d7398c7b
Ramiro Bueno Martínez <ramirobmar@gmail.com>
2018-05-07 16:52:14 +0200

    Herramienta de Visualización Paginas Web
    Parte Ejecutable de forma Parcial para visualizar y extraer código de páginas web

---
+++ WindowsExplorerForms/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace WindowsExplorerForms
+{
+    partial class xDSL_Tools
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(xDSL_Tools));
+            this.SuspendLayout();
+            // 
+            // xDSL_Tools
+            // 
+            this.ClientSize = new System.Drawing.Size(947, 381);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "xDSL_Tools";
+            this.Text = "xDSL Tools";
+            this.Load += new System.EventHandler(this.xDSL_Tools_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+    }
+}
+
---
+++ WindowsExplorerForms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Security.Permissions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WindowsExplorerForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new xDSL_Tools());
+        }
+    }
+}
